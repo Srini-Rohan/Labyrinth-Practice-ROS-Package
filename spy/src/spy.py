f8c900724f1d27a8a242e81be46ab5ce49802a76
SriniRohan <srinirohan31@gmail.com>
2022-03-01 20:36:29 +0530

    commit

--- spy/src/spy.py
+++ spy/src/spy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import cv2
+import os
 import rospy
 from coordi_msgs.msg import coordi
 from nav_msgs.msg import Odometry
@@ -25,13 +26,14 @@ if __name__ == '__main__':
     rospy.init_node('spy', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
+        path=os.path.dirname(os.path.realpath(__file__))
         location=coordi()
         if x<-4.5 and x>-5.5 and y<-8.1 and y>-9.1 and aruco==0:
-            location.image=cv2.imread('/home/srinir/catkin_ws/src/Labyrinth-22/spy/src/images/image1.png').reshape(-1)
+            location.image=cv2.imread(path+'/images/image1.png').reshape(-1)
             location.orientation=[0,0,0.7,0.7]
             pub.publish(location)
         if x<8.6 and x>7.6 and y<-5.4 and y>-6.4 and aruco==10:
-            location.image=cv2.imread('/home/srinir/catkin_ws/src/Labyrinth-22/spy/src/images/image2.png').reshape(-1)
+            location.image=cv2.imread(path+'/images/image2.png').reshape(-1)
             location.orientation=[0,0,0.7,0.7]
             pub.publish(location)
         rate.sleep()
